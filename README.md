c7d7bfd6154a2de6b5bdec69d81f4b3db3ae5b8f
Søren Engel <soren.engel@gmail.com>
2017-04-21 14:22:33 +0200

    Added initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Elmah.Io.Sitecore.log4net
+
+Log to elmah.io from the default Sitecore log4net implementation.
+
+## Installation
+
+First, make sure that you have installed the [elmah.io NuGet package](https://www.nuget.org/packages/elmah.io/) into your solution, and that you have added all of the necessary configuration to the Sitecore Web.config file. 
+
+> For more information on how to install elmah.io, please refer to the section [Configure elmah.io manually](https://docs.elmah.io/configure-elmah-io-manually/) found on [elmah.io documentation](https://docs.elmah.io/).
+
+Once installed, you need to configure your elmah.io log ID in the configuration file, found under ``App_Config/Include/Elmah.io.Sitecore.log4net.config``.
---
+++ src/Elmah.io.Sitecore.log4net/ElmahIoAppender.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Elmah.Io.Client;
+using log4net.Appender;
+using log4net.helpers;
+using log4net.spi;
+
+namespace Elmah.io.Sitecore.log4net
+{
+	public class ElmahIoAppender : AppenderSkeleton
+	{
+		public Elmah.Io.Client.ILogger Logger;
+		private Guid _logId;
+
+		public string LogId
+		{
+			set
+			{
+				if (!Guid.TryParse(value, out _logId))
+					throw new ArgumentException("LogId is not a GUID");
+			}
+		}
+
+		public string Application { get; set; }
+
+		protected override void Append(LoggingEvent loggingEvent)
+		{
+			if (Logger == null)
+				Logger = new Elmah.Io.Client.Logger(_logId);
+
+			var data = PropertiesToData(loggingEvent.Properties);
+
+			var message = new Message(loggingEvent.RenderedMessage)
+			{
+				Severity = LevelToSeverity(loggingEvent.Level),
+				DateTime = loggingEvent.TimeStamp.ToUniversalTime(),
+				Detail = loggingEvent.GetExceptionStrRep(),
+				Data = data,
+				Application = Application ?? loggingEvent.Domain,
+				Source = loggingEvent.LoggerName,
+				User = loggingEvent.UserName
+			};
+
+			Logger.Log(message);
+		}
+
+		private static List<Item> PropertiesToData(PropertiesCollection properties)
+		{
+			return properties
+				.GetKeys()
+				.Select(key => new Item { Key = key, Value = properties[key].ToString() })
+				.ToList();
+		}
+
+		private static Severity? LevelToSeverity(Level level)
+		{
+			if (level == Level.EMERGENCY)
+				return Severity.Fatal;
+			if (level == Level.FATAL)
+				return Severity.Fatal;
+			if (level == Level.ALERT)
+				return Severity.Fatal;
+			if (level == Level.CRITICAL)
+				return Severity.Fatal;
+			if (level == Level.SEVERE)
+				return Severity.Fatal;
+			if (level == Level.ERROR)
+				return Severity.Error;
+			if (level == Level.WARN)
+				return Severity.Warning;
+			if (level == Level.NOTICE)
+				return Severity.Information;
+			if (level == Level.INFO)
+				return Severity.Information;
+			if (level == Level.DEBUG)
+				return Severity.Debug;
+			if (level == Level.FINE)
+				return Severity.Verbose;
+			if (level == Level.TRACE)
+				return Severity.Verbose;
+			if (level == Level.FINER)
+				return Severity.Verbose;
+			if (level == Level.VERBOSE)
+				return Severity.Verbose;
+			if (level == Level.FINEST)
+				return Severity.Verbose;
+
+			return Severity.Information;
+		}
+	}
+}
